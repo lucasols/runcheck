e41c612fb27195920b1c3412d4b9e2b9acd4d723
lucaslos <lucas@sugestly.com>
2023-08-31 13:24:32 -0300

    fix types

--- src/runcheck.ts
+++ src/runcheck.ts
@@ -64,6 +64,7 @@ export type RcBase<T, RequiredKey extends boolean> = {
     customAutofix: (input: unknown) => false | { fixed: T },
   ) => RcType<T>
 
+  /** @internal */
   readonly _optional_key_?: RequiredKey
   /** @internal */
   readonly _parse_: (
