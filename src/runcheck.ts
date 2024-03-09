a0c31c44790d3e51e5e56ea75b764d3dcc0e537c
lucaslos <lucas@sugestly.com>
2024-03-09 20:02:59 -0300

    improve types

--- src/runcheck.ts
+++ src/runcheck.ts
@@ -1056,7 +1056,7 @@ export function rc_unwrap_or_null<R>(result: RcParseResult<R>): {
   return rc_unwrap_or(result, null)
 }
 
-export function rc_unwrap_or<R, F>(
+export function rc_unwrap_or<R, F = R>(
   result: RcParseResult<R>,
   fallback: F,
 ): {
