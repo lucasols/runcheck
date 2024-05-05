348abeab6d50a400c345a1451225a57dd95ffd47
lucaslos <lucas@sugestly.com>
2024-05-05 18:32:15 -0300

    chore: Update tsup.config.ts to only generate ES modules

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,13 @@
+import { writeFileSync } from 'fs'
 import { defineConfig } from 'tsup'
 import pkg from './package.json'
-import { writeFileSync } from 'fs'
 
 writeFileSync(`./last-build-version.txt`, `v${pkg.version}\n`, 'utf-8')
 
 export default defineConfig({
   entry: ['src/runcheck.ts', 'src/autofixable.ts'],
   clean: true,
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   sourcemap: true,
   esbuildOptions(options) {
     options.mangleProps = /[^_]_$/
